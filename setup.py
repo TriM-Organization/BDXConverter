73409e724bb809c60cf59dd7738122d8a8d96e4d
Happy2018new <chenbo79800@163.com>
2023-05-02 11:47:26 +0800

    update `README.md` and bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ file.close()
 
 setuptools.setup(
     name="BDXConverter",
-    version="1.0.2",
+    version="1.0.3",
     author="Minecraft Muti-Media Organization",
     author_email="TriM-Organization@hotmail.com",
     description="A code library to marshal, unmarshal, visual and reverse visualization of BDX files",
