fa7eadbc135cb0093d5a7ecf0c6bcf95ff262d7b
Happy2018new <chenbo79800@163.com>
2023-05-03 15:45:51 +0800

    uniform format

--- setup.py
+++ setup.py
@@ -1,28 +1,28 @@
 import setuptools
 
-with open("requirements.txt", "r+", encoding="utf-8") as file:
-    dependences = file.read().strip().split("\n")
+with open('requirements.txt', 'r+', encoding='utf-8') as file:
+    dependences = file.read().strip().split('\n')
 
-with open("README.md", "r+", encoding='utf-8') as file:
+with open('README.md', 'r+', encoding='utf-8') as file:
     long_description = file.read()
 
-with open("version", "r+", encoding='utf-8') as file:
+with open('version', 'r+', encoding='utf-8') as file:
     version = file.read()
 
 setuptools.setup(
-    name="BDXConverter",
+    name='BDXConverter',
     version=version,
-    author="Minecraft Muti-Media Organization",
-    author_email="TriM-Organization@hotmail.com",
-    description="A code library to marshal, unmarshal, visual and reverse visualization of BDX files",
+    author='Minecraft Muti-Media Organization',
+    author_email='TriM-Organization@hotmail.com',
+    description='A code library to marshal, unmarshal, visual and reverse visualization of BDX files',
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/TriM-Organization/BDXConverter",
+    long_description_content_type='text/markdown',
+    url='https://github.com/TriM-Organization/BDXConverter',
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
     ],
     install_requires=dependences,
     python_requires='>=3.9',
